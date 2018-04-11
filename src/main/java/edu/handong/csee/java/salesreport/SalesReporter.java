55aef253d5ee098af60facded8e9329f6c6708ea
YuJin-Kim <21700140@handong.edu>
2018-04-12 03:14:06 +0900

    add javadoc

--- src/main/java/edu/handong/csee/java/salesreport/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/salesreport/SalesReporter.java
@@ -2,6 +2,16 @@ package edu.handong.csee.java.salesreport;
 
 import java.util.Scanner;
 
+/**
+ * This class define SalesReporter object. </br>
+ * The SalesReporter class has five members.</br>
+ * => highestSales, averageSales, highestSalesIndex, team and numberOfAssociates</br>
+ * The SalesReporter class has getData(), computeStats() and displayResults() methods.</br>
+ * 
+ * @author YuJin
+ *
+ */
+
 public class SalesReporter {
 	private double highestSales;
 	private double averageSales=0;
@@ -9,66 +19,71 @@ public class SalesReporter {
 	private SalesAssociate[] team;
 	private int numberOfAssociates; // same as team length
 	
-	public void getData(SalesReporter salesAssociates) {
+	public void getData() {
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("Enter number of sales associates:");
-		salesAssociates.numberOfAssociates=keyboard.nextInt();
+		numberOfAssociates=keyboard.nextInt();
 		
-		team = new SalesAssociate [salesAssociates.numberOfAssociates];
+		team = new SalesAssociate [numberOfAssociates];
 		
 		for (int count=0; count < team.length; count++) {
+			team[count] = new SalesAssociate();
+			keyboard.nextLine();
 			System.out.println("Enter data for associate number "+(count+1));
 			System.out.print("Enter name of sales associate: ");
-			team[count].name=keyboard.next();
+			team[count].name=keyboard.nextLine();
 			System.out.print("Enter associate's sales: $");
 			team[count].sales=keyboard.nextDouble();
 		}
 	}
 	
-	public void computeStats(SalesReporter salesAssociates) {
+	public void computeStats() {
 		
-		salesAssociates.highestSales=team[0].sales;
+		highestSales=team[0].sales;
 		
 		for (int count=1; count<team.length; count++)
-			if (salesAssociates.highestSales<team[count].sales) {
-				salesAssociates.highestSales=team[count].sales;
-				salesAssociates.highestSalesIndex=count;
+			if (highestSales<team[count].sales) {
+				highestSales=team[count].sales;
+				highestSalesIndex=count;
 			}
 		
 		for(int count=0; count<team.length; count++)
-			salesAssociates.averageSales+=team[count].sales;
+			averageSales+=team[count].sales;
 		
-		salesAssociates.averageSales=salesAssociates.averageSales/3.0;
+		averageSales=averageSales/3.0;
 	}
 	
-	public void displayResults(SalesReporter salesAssociates) {
-		System.out.println("Average sales per associate is $"+salesAssociates.averageSales);
-		System.out.println("The highest sales figure is $"+salesAssociates.highestSales);
+	public void displayResults() {
+		System.out.println("Average sales per associate is $"+averageSales);
+		System.out.println("The highest sales figure is $"+highestSales);
 		System.out.println("The following had the highest sales:");
-		System.out.println("Name: "+team[salesAssociates.highestSalesIndex].name);
-		System.out.println("Sales: $"+team[salesAssociates.highestSalesIndex].sales);
-		System.out.println("$"+"below the average.");
+		System.out.println("Name: "+team[highestSalesIndex].name);
+		System.out.println("Sales: $"+team[highestSalesIndex].sales);
+		System.out.println("$"+(team[highestSalesIndex].sales-averageSales)+" above the average.");
 		System.out.println("The rest performed as follows:");
 		for(int count=0; count<team.length; count++) {
-			if (count==salesAssociates.highestSalesIndex)
+			if (count==highestSalesIndex)
 				continue;
 			else {
-				System.out.println("Name: "+team[salesAssociates.highestSalesIndex].name);
-				System.out.println("Sales: $"+team[salesAssociates.highestSalesIndex].sales);
-				System.out.println("$"+"below the average.");
+					System.out.println("Name: "+team[count].name);
+					System.out.println("Sales: $"+team[count].sales);
+					if (team[count].sales>averageSales)
+						System.out.println("$"+(team[count].sales-averageSales)+" above the average.");
+					else
+						System.out.println("$"+(averageSales-team[count].sales)+" below the average.");
 			}
 		}
 	}
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		SalesReporter salesAssociates = new SalesReporter();
 
-		getData(salesAssociates);
-		computeStats(salesAssociates);
-		displayResults(salesAssociates);
+		salesAssociates.getData();
+		salesAssociates.computeStats();
+		salesAssociates.displayResults();
 	}
 
 }
